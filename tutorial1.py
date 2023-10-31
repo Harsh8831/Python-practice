ee4f722a387c01d987f0d722dafca84693d666ab
Harsh Kumar Gupta <145157174+Harsh8831@users.noreply.github.com>
2023-10-31 21:22:22 +0530

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -5,3 +5,32 @@ x= 3
 y= "hello harsh bhai"
 print(type(x))
 print(type(y))
+
+#rules for declsring variable names
+# 1 variable name must start with letters and underscore
+# 2 variable name cannot start with a number
+# 3 variable name can only contain alpha-numeric characters and underscore
+# 4 variable names are case sensesitve (it means x and X are two different names)
+# 5 variable cannot be a python keyword
+
+#CASTING- specifying the data types of a variable is known as casting
+print("casting")
+
+a = str(8)
+b = int(9)
+c = float(10)
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Multi words variable names
+print("Multi word variable name")
+
+# camel case
+variableName = "each word except first word start with capital letter"
+
+# pascal case
+VariableName = "each word start with a capital letter"
+
+# Snake case
+variable_name = "each word is separated by an underscore character"
